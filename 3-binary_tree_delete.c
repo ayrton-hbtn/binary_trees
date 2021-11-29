c86f84892edbec49876443b92e73034e2df3ad24
Marcelo <3338@holbertonschool.com>
2021-11-29 08:27:03 -0800

    delete binary tree

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,8 +1,9 @@
 #include "binary_trees.h"
-/*
- * *
- */
 
+/**
+ * binary_tree_delete - deletes a binary tree
+ * @tree: tree to delete
+ */
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree != NULL)
@@ -10,5 +11,5 @@ void binary_tree_delete(binary_tree_t *tree)
 		binary_tree_delete(tree->right);
 		binary_tree_delete(tree->left);
 		free(tree);
-	}	
+	}
 }
