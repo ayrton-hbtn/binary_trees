e13bbe73aafc4964ca2577c173f51222783db31d
hippocampus3282 <3282@holbertonschool.com>
2021-11-29 10:55:49 -0800

    height of tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,33 +1,39 @@
 #include "binary_trees.h"
 
 /**
+  * max - returns max between two numbers
+  * @a: first number
+  * @b: second number
+  * Return: max between a and b
+  */
+size_t max(size_t a, size_t b)
+{
+	if (a > b)
+		return (a);
+	return (b);
+}
+
+/**
+  * get_height - measures the height of a binary tree
+  * @tree: tree to measure
+  * Return: height of tree, 0 if tree is NULL
+  */
+size_t get_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (1 + max(get_height(tree->left), get_height(tree->right)));
+}
+
+/**
   * binary_tree_height - measures the height of a binary tree
   * @tree: tree to measure
   * Return: height of tree, 0 if tree is NULL
   */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height_l = 0, height_r = 0;
-	const binary_tree_t *tmp = tree;
+	size_t height = get_height(tree);
 
-	if (tree != NULL)
-	{
-		while (tmp->left)
-		{
-			tmp = tmp->left;
-			height_l++;
-		}
-
-		tmp = tree;
-
-		while (tmp->right)
-		{
-			tmp = tmp->right;
-			height_r++;
-		}
-	}
-
-	if (height_l >= height_r)
-		return (height_l);
-	return (height_r);
+	return (height - 1);
 }
