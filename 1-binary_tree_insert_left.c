7b5e29647d54f7952a4bbb5af048f623ddabd934
hippocampus3282 <3282@holbertonschool.com>
2021-11-30 13:44:10 -0800

    fix insert_left & insert_right

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,33 +2,29 @@
 
 /**
   * binary_tree_insert_left - inserts a new node as left child
-  * @parent: parent node to insert new node at left
+  * @parent: parent node to insert new node to
   * @value: value of new node
+  * Description: If parent node already has a left child, the new node
+  *     must take its place, and the old left-child must be set
+  *     as the left-child of the new node.
   * Return: new node, NULL if failed or if parent doesn't exist
   */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new, *l_child = parent->left;
 
+	if (!parent)
+		return (NULL);
+
+	new = binary_tree_node(parent, value);
 	if (!new)
 		return (NULL);
 
-	if (parent == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
+	if (l_child)
+		l_child->parent = new;
 
-	new->n = value;
-	new->parent = parent;
-
-	if (parent->left != NULL)
-	{
-		parent->left->parent = new;
-		new->left = parent->left;
-	}
+	new->left = l_child;
 	parent->left = new;
-	new->parent = parent;
 
 	return (new);
 }
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -2,33 +2,29 @@
 
 /**
   * binary_tree_insert_right - inserts a new node as right child
-  * @parent: parent node to insert new node at right
+  * @parent: parent node to insert new node to
   * @value: value of new node
+  * Description: If parent node already has a right child, the new node
+  *	must take its place, and the old right-child must be set
+  *	as the right-child of the new node.
   * Return: new node, NULL if failed or if parent doesn't exist
   */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new, *r_child = parent->right;
 
+	if (!parent)
+		return (NULL);
+
+	new = binary_tree_node(parent, value);
 	if (!new)
 		return (NULL);
 
-	if (parent == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
+	if (r_child)
+		r_child->parent = new;
 
-	new->n = value;
-	new->parent = parent;
-
-	if (parent->right != NULL)
-	{
-		parent->right->parent = new;
-		new->right = parent->right;
-	}
+	new->right = r_child;
 	parent->right = new;
-	new->parent = parent;
 
 	return (new);
 }
