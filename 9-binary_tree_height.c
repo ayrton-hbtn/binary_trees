d9a52b5b28acb63a47c514ca2cc03a7ac21be6aa
hippocampus3282 <3282@holbertonschool.com>
2021-12-01 08:00:25 -0800

    change variable name

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,19 +7,19 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height_left = 0, height_right = 0;
+	size_t l_height = 0, r_height = 0;
 
 	if (tree)
 	{
 		if (tree->left)
-			height_left = binary_tree_height(tree->left) + 1;
+			l_height = binary_tree_height(tree->left) + 1;
 		if (tree->right)
-			height_right = binary_tree_height(tree->right) + 1;
+			r_height = binary_tree_height(tree->right) + 1;
 
-		if (height_left >= height_right)
-			return (height_left);
+		if (l_height >= r_height)
+			return (l_height);
 		else
-			return (height_right);
+			return (r_height);
 	}
 	else
 		return (0);
