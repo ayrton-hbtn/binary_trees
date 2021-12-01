7c3619e5d3a216a152aa30a3f6d07b3c76181953
Marcelo <3338@holbertonschool.com>
2021-12-01 14:47:48 -0800

    110 fix

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -15,13 +15,15 @@ int binary_tree_is_bst_valid(binary_tree_t *tree, int nodeVal, int lessThan)
 		{
 			if (tree->n >= nodeVal)
 				return (0);
-			return (binary_tree_is_bst_valid(tree->left, nodeVal, lessThan) && binary_tree_is_bst_valid(tree->right, nodeVal, lessThan));
+			return (binary_tree_is_bst_valid(tree->left, nodeVal, lessThan) &&
+					binary_tree_is_bst_valid(tree->right, nodeVal, lessThan));
 		}
 		else
 		{
 			if (tree->n <= nodeVal)
 				return (0);
-			return (binary_tree_is_bst_valid(tree->left, nodeVal, lessThan) && binary_tree_is_bst_valid(tree->right, nodeVal, lessThan));
+			return (binary_tree_is_bst_valid(tree->left, nodeVal, lessThan) &&
+					binary_tree_is_bst_valid(tree->right, nodeVal, lessThan));
 		}
 	}
 	return (1);
@@ -35,7 +37,8 @@ int binary_tree_is_bst(const binary_tree_t *tree)
 {
 	if (tree != NULL)
 	{
-		if (binary_tree_is_bst_valid(tree->left, tree->n, 1) && binary_tree_is_bst_valid(tree->right, tree->n, 0))
+		if (binary_tree_is_bst_valid(tree->left, tree->n, 1) &&
+				binary_tree_is_bst_valid(tree->right, tree->n, 0))
 			return (binary_tree_is_bst(tree->left) && binary_tree_is_bst(tree->right));
 		else
 			return (0);
