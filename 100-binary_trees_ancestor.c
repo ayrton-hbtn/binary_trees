bfd5388ffc55423ea8773c14aaa4e2dd434b020b
hippocampus3282 <3282@holbertonschool.com>
2021-12-01 08:06:29 -0800

    fix

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,6 +1,31 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - function that measures the height of a binary tree
+ * @tree: pointer to the root node
+ * Return: height of binary tree, 0 if NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t l_height = 0, r_height = 0;
+
+	if (tree)
+	{
+		if (tree->left)
+			l_height = binary_tree_height(tree->left) + 1;
+		if (tree->right)
+			r_height = binary_tree_height(tree->right) + 1;
+
+		if (l_height >= r_height)
+			return (l_height);
+		else
+			return (r_height);
+	}
+	else
+		return (0);
+}
+
+/**
   * binary_trees_ancestor - finds the lowest common ancestor of two nodes
   * @first: first node
   * @second: second node
