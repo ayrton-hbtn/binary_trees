d52b57ccc7b3732102b6246783dd2fba0b389a1f
Marcelo <3338@holbertonschool.com>
2021-12-01 06:15:28 -0800

    1 fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,6 +16,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (!parent)
 		return (NULL);
 
+	l_child = parent->left;
 	new = binary_tree_node(parent, value);
 	if (!new)
 		return (NULL);
