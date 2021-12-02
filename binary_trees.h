d370c9af5a0679bb0584963e83fda7ab2b539b10
Marcelo <3338@holbertonschool.com>
2021-12-02 11:21:51 -0800

    .h

--- binary_trees.h
+++ binary_trees.h
@@ -22,6 +22,8 @@ struct binary_tree_s
 
 typedef struct binary_tree_s binary_tree_t;
 
+typedef struct binary_tree_s bst_t;
+
 /* PROTOTYPES */
 void binary_tree_print(const binary_tree_t *tree);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
@@ -48,5 +50,6 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
+bst_t *bst_remove(bst_t *root, int value);
 
 #endif /* BINARY_TREES_H */
